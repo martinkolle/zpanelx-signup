b5da60321bf22def91dea5eb1c54e9ed3453f7a4
martinkolle <martin.kollerup@gmail.com>
2012-07-13 01:48:56 +0200

    api integrated

--- pay.php
+++ pay.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- *  Functions for zpanelx Auto-sign-up
+ *  Pay-page for zpanelx Auto-sign-up
  *  
  *  @package    Zpanelx Auto-sign-up
  *  @author     Tony Maclennan & Martin Kollerup
  *  @license    http://opensource.org/licenses/gpl-3.0.html
  */
 
-include ('db.php');
-include ('config/functions.php');
+include ('lib/functions.php');
+include ('lib/xmwsclient.class.php');
 
 $token = $_GET['id'];
 $error = array();
@@ -21,92 +21,96 @@ if(!isset($token)){
      $error[] = "No invoice id have been set";
 }
 
-//connect to the databse
-$db = db::getConnection();
-$stmt = $db->prepare("SELECT * FROM x_invoice WHERE token= ?");
-    
-$stmt->execute(array($token));
-$row = $stmt->fetch();
-$invpaid = $row['inv_payment_id'];
-$amount = $row['inv_amount'];
-$invoiceuser = $row['inv_user'];
+if(preg_match("/^[a-zA-Z0-9]+$/", $token) == TRUE){
 
-if(!$row['token']){
-     //we don't need to load the page right now
-     die("Invoice id was not found in the system."); 
+     $xmws = new xmwsclient();
+     $xmws->InitRequest(zpanelx::getConfig('zpanel_url'), 'reseller_billing', 'Invoice', zpanelx::getConfig('api'));
+     $xmws->SetRequestData('<token>'.$token.'</token>');
+     $return = $xmws->XMLDataToArray($xmws->Request($xmws->BuildRequest()), 0);
+     
+     if (!empty($return['xmws']['content']['invoice']['id'])) {
+          $inv_user      = $return['xmws']['content']['invoice']['user'];
+          $inv_amount    = $return['xmws']['content']['invoice']['amount'];
+          $inv_paid      = $return['xmws']['content']['invoice']['payment_id'];
+          $inv_id        = $return['xmws']['content']['invoice']['id'];
+     } 
+     else{
+          if(zpanelx::getConfig('DEBUG')){
+               $error[] = "Error getting invoice data";
+          }
+     }
+} else {
+     $error[] = "Invoice token is invalid";
 }
-//if the invoice have been paid - return this.
-if ($invpaid != 'no') {
-     $error[] = "This invoice has already been paid.";
+
+
+if(empty($error)){
+     if(!$inv_user){
+          //we don't need to load the page right now
+          $error[] = "Invoice id was not found in the system."; 
+     } 
+     elseif($inv_paid != 'no'){
+          $error[] = "This invoice has already been paid.";
+     }
 }
 
 if(empty($error)){
 
-     $stmt = $db->prepare("SELECT * FROM x_accounts WHERE ac_id_pk= ?");
-    
-     $stmt->execute(array($invoiceuser));
-     $row = $stmt->fetch();
+     $xmws = new xmwsclient();
+     $xmws->InitRequest(zpanelx::getConfig('zpanel_url'), 'reseller_billing', 'Pay', zpanelx::getConfig('api'));
+     $xmws->SetRequestData('<profile_id>'.$inv_user.'</profile_id><account_id>'.$inv_user.'</account_id><payment>1</payment>');
+     $return = $xmws->XMLDataToArray($xmws->Request($xmws->BuildRequest()), 0);
      
-     $useralias = $row['ac_user_vc'];
-     $user_id = $row['ac_id_pk'];
-     $user_email = $row['ac_email_vc'];
-     $userpayperiod = $row['ac_invoice_period'];     
+     if (!empty($return['xmws']['content'])) {
+          $user_alias      = $return['xmws']['content']['account']['alias'];
+          $user_id         = $return['xmws']['content']['account']['id'];
+          $user_email      = $return['xmws']['content']['account']['email'];
+          $user_payperiod  = $return['xmws']['content']['account']['payperiod'];
 
-     //Load payment methods
-     $pmtemplate = file_get_contents('templates/paymethod.html');
-     $stmt = $db->prepare("SELECT * FROM x_payment_methods WHERE pm_active='1'");
-              
-     if($stmt->execute()){
-          foreach($stmt->fetchAll() as $row){
-               $pmtemp = $pmtemplate;
-               $pmtemp = str_replace('{{paycode}}',$row['pm_data'],$pmtemp);
-               $pmtemp = str_replace('{{payname}}',$row['pm_name'],$pmtemp);
-                    
-                    //incase it's a subscription, enter subsequent payment periods
-               if ($userpayperiod == "1") {
-                    $pmtemp = str_replace('$subscrper','1',$pmtemp);
-               } elseif ($userpayperiod == "2") {
-                    $pmtemp = str_replace('$subscrper','3',$pmtemp);
-               } elseif ($userpayperiod == "3") {
-                    $pmtemp = str_replace('$subscrper','12',$pmtemp);
-               }
-               $accdat .= $pmtemp;
+          $payments        = $return['xmws']['content']['payments']['payment'];
+
+          $profile_fullname= $return['xmws']['content']['profile']['fullname'];
+     } 
+     else{
+          if(zpanelx::getConfig('DEBUG')){
+               $error[] = "Error getting account data";
           }
      }
-     $stmt = $db->prepare("SELECT * FROM x_profiles WHERE ud_user_fk= ?");
-     $stmt->execute(array($user_id));
-     $user = $stmt->fetch();
 
-
+     //Generate the payment page
+     //TODO: new design - please!!
+     $template = file_get_contents('templates/paymethod.html');
+     foreach($payments as $row){
+          $pmtemp = $template;
+          $pmtemp = str_replace('{{paycode}}',$row['data'],$pmtemp);
+          $pmtemp = str_replace('{{payname}}',$row['name'],$pmtemp);
+          $form .= $pmtemp;
+     }
+ 
      $action = (zpanelx::getConfig('test')) ? 'https://www.sandbox.paypal.com/cgi-bin/webscr' : 'https://www.paypal.com/cgi-bin/webscr';
-     $accdat = str_replace('{{action}}', $action, $accdat);
-     $accdat = str_replace('{{user_firstname}}', $user['ud_fullname_vc'], $accdat);
-     $accdat = str_replace('{{invoice}}', $token, $accdat);
-     $accdat = str_replace('{{email}}', $user_email, $accdat);
-     $accdat = str_replace('{{return_url}}', zpanelx::getConfig('return_url'), $accdat);
-     $accdat = str_replace('{{business}}', zpanelx::getConfig('email_paypal'), $accdat);
-     $accdat = str_replace('{{item_name}}', "Webhosting", $accdat);//will have the oackage name and period in next release
-     $accdat = str_replace('{{country}}', zpanelx::getConfig('country_code'), $accdat);
-     $accdat = str_replace('{{amount}}', $amount, $accdat);
-     $accdat = str_replace('{{logo}}', zpanelx::getConfig('logo'), $accdat);
-     $accdat = str_replace('{{notify_url}}', zpanelx::getConfig('notify_url'), $accdat);
-     $accdat = str_replace('{{cs}}', zpanelx::getConfig('cs'), $accdat);
+     $form = str_replace('{{action}}', $action, $form);
+     $form = str_replace('{{user_firstname}}', $user['ud_fullname_vc'], $form);
+     $form = str_replace('{{invoice}}', $token, $form);
+     $form = str_replace('{{email}}', $user_email, $form);
+     $form = str_replace('{{return_url}}', zpanelx::getConfig('return_url'), $form);
+     $form = str_replace('{{business}}', zpanelx::getConfig('email_paypal'), $form);
+     $form = str_replace('{{item_name}}', "Webhosting", $form);//will have the oackage name and period in next release
+     $form = str_replace('{{country}}', zpanelx::getConfig('country_code'), $form);
+     $form = str_replace('{{amount}}', $inv_amount, $form);
+     $form = str_replace('{{logo}}', zpanelx::getConfig('logo'), $form);
+     $form = str_replace('{{notify_url}}', zpanelx::getConfig('notify_url'), $form);
+     $form = str_replace('{{cs}}', zpanelx::getConfig('cs'), $form);
 
-          $html['pay'] .= "Paying today: " . zpanelx::getConfig('cs') ." ". $amount;
-          $html['body'] .= $accdat; 
-          $html['title'] = "Pay for hosting";
+     $html['pay'] .= "Paying today: " . zpanelx::getConfig('cs') ." ". $inv_amount;
+     $html['body'] .= $form; 
+     $html['title'] = "Pay for hosting";
 }//end if no error
 
-          if ($accstatus =="0") {
-               include 'templates/pay_acc_inactive.html';
-          }
-          $html['body'] .= implode("<br/>", $error);
-          $template = file_get_contents('templates/pay.html');
-          $template = str_replace('{{title}}', $html['title'], $template);
-          $template = str_replace('{{body}}', $html['body'], $template);
-          $template = str_replace('{{pay}}', $html['pay'], $template);
+     $html['body'] .= implode("<br/>", $error);
+     $template = file_get_contents('templates/pay.html');
+     $template = str_replace('{{title}}', $html['title'], $template);
+     $template = str_replace('{{body}}', $html['body'], $template);
+     $template = str_replace('{{pay}}', $html['pay'], $template);
 
-
-          echo $template;
-
+     echo $template;
 ?>
