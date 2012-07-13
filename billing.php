91851bc6e99ba22e871e6ee6f4b6aba6c159b9bd
martinkolle <martin.kollerup@gmail.com>
2012-07-13 02:35:45 +0200

    Api integration

--- billing.php
+++ billing.php
@@ -1,20 +1,25 @@
 <?php
-// Developed by Tony Maclennan (tonymaclennan@hotmail.com)
-// Developed for ZPanelCP8
-// Not for resale
+
+/**
+ *  Functions for zpanelx Auto-sign-up
+ *  
+ *  @package    Zpanelx Auto-sign-up
+ *  @author     Martin Kollerup
+ *  @license    http://opensource.org/licenses/gpl-3.0.html
+ */
+
 define('zxBilling', 1);
 
-include 'config/functions.php';
-include ('db.php');
-include 'db_connect.php';
-//include 'config/config.php';
+include ('lib/functions.php');
+include ('lib/db.php');
+include ('lib/xmwsclient.class.php');
 
 $pid = $_GET['pid'];
 //connect to the database
 $db = db::getConnection();
 
-//If form has been submitted, action it
-if (isset($_POST['SubmitForm'])) {
+//If $form has been submitted, action it
+if (isset($_POST['Submit$form'])) {
 
 	$error 		= array();
 	$username 	= filter_var($_POST["username"],FILTER_SANITIZE_STRING);
@@ -26,12 +31,11 @@ if (isset($_POST['SubmitForm'])) {
 
 	$postcode 	= ($_POST["postcode"]);
 	$telephone 	= ($_POST["telephone"]);
-	//TODO: IS THE INFORMATIONS RIGHT
+	//TODO: IS THE IN$formATIONS RIGHT
 	
-	$payperiod 	= $_POST['payperiod'];
-	$packageid 	= $_POST['packageid'];
+	$payperiod 	= ($_POST['payperiod']);
+	$packageid 	= ($_POST['packageid']);
 	$token 		= zpanelx::generateToken();
-
 
 	//start by checking for missing inputs
 	if (empty($username)) {
@@ -53,39 +57,49 @@ if (isset($_POST['SubmitForm'])) {
 	}
 	if (empty($postcode)) {
 		$error[] = "Postcode missing";
+	} else {
+		if(preg_match("/^([1]-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$/i",$postcode)){
+			$error[] = "Telephone is not valid";
+		}
 	}
 	if (empty($telephone)) {
 		$error[] = "Telephone number missing";
+	} else {
+		if(preg_match("/^([1]-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$/i",$telephone)){
+			$error[] = "Telephone is not valid";
+		}
 	}
 	if (empty($payperiod)) {
 		$error[] = "Payperiod is missing";
+	} else {
+		if(preg_match("/^([1]-)?[0-9]{3}-[0-9]{3}-[0-9]{4}$/i",$payperiod)){
+			$error[] = "Telephone is not valid";
+		}
 	}
 
-	$stmt = $db->prepare("SELECT * FROM x_accounts WHERE ac_user_vc= ?");
-		
-	if($stmt->execute(array($username))){
-		$row = $stmt->fetch();
-		$user_exits = $row['ac_email_vc'];
-	}
-
-	if (isset($user_exits)) {
-		$error[] = "Username allready exits. Please choose a new!";
+	$xmws = new xmwsclient();
+	$xmws->InitRequest(zpanelx::getConfig('zpanel_url'), 'reseller_billing', 'UsernameExits', zpanelx::getConfig('api'));
+	$xmws->SetRequestData('<username>'.$username.'</username>');
+	$returnUsername = $xmws->XMLDataToArray($xmws->Request($xmws->BuildRequest()), 0);
+	
+	if ($returnUsername['xmws']['content']['code'] != 3) {
+		$error[] = $returnUsername['xmws']['content']['human'];
 	}
 
 	//If there not are any erorrs.. proceed.
-	//TODO: Add the påassword when the payment is accepted
+	//TODO: Add the password when the payment is accepted
 	if(!$error){
 		//add the new user
 		//use "newUser" if you don't want to use the API!!!
 		zpanelx::newUser2($payperiod, $packageid, $token, zpanelx::generatePassword(), $username, $email, $fullname, $adress, $postcode, $telephone);
 		
+		//Something went wrong.. The user will be in$formed - turn debug on to find the problem!
 		if(isset(zpanelx::$newUserError)){
 			$error = "It is currently not possible to crate your account. A notification have been sent to the provider.<br /><b>We are sorry!</b>";
 			$template = file_get_contents('templates/billing_error.html');
 			$template = str_replace('{{title}}', "Errors", $template);
 			$template = str_replace('{{error}}', $error, $template);
 		}
-		//the user will be redirected from the function
 	}
 	else{
 		$error = implode("<br/>",$error);
@@ -95,34 +109,41 @@ if (isset($_POST['SubmitForm'])) {
 //Firstly read URL to get package and user type
 if(!empty($pid)){
 
-	//select package informations
-	$stmt = $db->prepare("SELECT * FROM x_packages WHERE pk_id_pk= ?");
-		
-	if($stmt->execute(array($pid))){
-		$row = $stmt->fetch();
-		$selectedpackagename = $row['pk_name_vc'];
-		$pricepm = $row['pk_price_pm'];
-		$pricepq = $row['pk_price_pq'];
-		$pricepy = $row['pk_price_py'];
+
+	$xmws = new xmwsclient();
+	$xmws->InitRequest(zpanelx::getConfig('zpanel_url'), 'reseller_billing', 'Package', zpanelx::getConfig('api'));
+	$xmws->SetRequestData('<pk_id>'.$pid.'</pk_id>');
+	$return = $xmws->XMLDataToArray($xmws->Request($xmws->BuildRequest()), 0);
+	
+	if (!empty($return['xmws']['content']['package']['id'])) {
+		$package_name 	= $return['xmws']['content']['package']['name'];
+		$price_pm 		= $return['xmws']['content']['package']['pm'];
+		$price_pq 		= $return['xmws']['content']['package']['pq'];
+		$price_py 		= $return['xmws']['content']['package']['py'];
+	} 
+	else{
+		if(zpanelx::getConfig('DEBUG')){
+			$error[] = "Error getting the package in$formations";
+		}
 	}
 
 	//we need to ensure that the package exits.
-	if(!empty($selectedpackagename)){
+	if(!empty($package_name)){
 		//add payments options
-		if (!$pricepm == "0") {
-			$loadpayoptions = '<input type="radio" name="payperiod" selected value="1">Monthly @ ' . $cs . $pricepm . '</input><br />';
+		if ($price_pm != "0") {
+			$payoptions = '<input type="radio" name="payperiod" selected value="1">Monthly @ '.zpanelx::getConfig('cs')." ".$price_pm .'</input><br />';
 		}
-		if (!$pricepq == "0") {
-			$loadpayoptions .= "<input type=\"radio\" name=\"payperiod\" value=\"2\">Quarterly @ " . $cs . $pricepq . "</input><br />";
+		if ($price_pq != "0") {
+			$payoptions .= "<input type=\"radio\" name=\"payperiod\" value=\"2\">Quarterly @ ".zpanelx::getConfig('cs')." ".$price_pq."</input><br />";
 		}
-		if (!$pricepy == "0") {
-			$loadpayoptions .= "<input type=\"radio\" name=\"payperiod\" value=\"3\">Yearly @ " . $cs . $pricepy . "</input><br />";
+		if ($price_py != "0") {
+			$payoptions .= "<input type=\"radio\" name=\"payperiod\" value=\"3\">Yearly @ ".zpanelx::getConfig('cs')." ".$price_py."</input><br />";
 		}
 
 		$template = file_get_contents('templates/billing.html');
 		$template = str_replace(':action', htmlspecialchars($_SERVER['SCRIPT_NAME'] .'?'. $_SERVER['QUERY_STRING']), $template);
-		$template = str_replace(':selectedpackagename', htmlentities($selectedpackagename, ENT_QUOTES), $template);
-		$template = str_replace(':payoptions', $loadpayoptions, $template);
+		$template = str_replace(':selectedpackagename', htmlentities($packagename, ENT_QUOTES), $template);
+		$template = str_replace(':payoptions', $payoptions, $template);
 		$template = str_replace(':pid', $pid, $template);
 		$template = str_replace(':title', "Buy hosting", $template);
 
