ac08a701eb34bd323dcaf6f5c2b2689b947c91a5
martinkolle <martin.kollerup@gmail.com>
2012-09-19 22:13:45 +0200

    New header, problems with different arrays,

--- index.php
+++ index.php
@@ -1,40 +1,44 @@
 <?php
 
 /**
- *  Main page zpanelx Auto-sign-up
- *  
- *  @package    Zpanelx Auto-sign-up
- *  @author     MaouKami
- *  @license    http://opensource.org/licenses/gpl-3.0.html
+ * List all packages
+ *
+ * @author MaouKami
+ * @copyright MaouKami
+ * @link http://www.kmweb.dk/
+ * @license GPL (http://www.gnu.org/licenses/gpl.html)
  */
-
 include ('lib/functions.php');
-include ('lib/xmwsclient.class.php');
 
 $head = null;
 
+    //Get packages from zpanelx
 	$listPackages = zpanelx::api("reseller_billing", "PackageList", "", zpanelx::getConfig('zpanel_url'), zpanelx::getConfig('api'));
-    //print_r($listPackages);
+
+    //We need to check that the array is the right. xmws.class is gennerating different arrays based on the number of packages. 
     $listPackages = (is_array($listPackages['xmws']['content']['package'][0])) ? $listPackages['xmws']['content']['package'] : $listPackages['xmws']['content'];
     foreach($listPackages as $row){
 
+        //List the prices for the package and find the cheapest
         $json = json_decode($row['hosting'], true);
         $price = array();
         foreach($json['hosting'] as $host){
             array_push($price, $host['price']);
         }
         $price = min($price);
-
-          $packetlist = file_get_contents('templates/packagelist.html');
-          if(preg_match('/^\d+$/', $_GET['id']) && $_GET['id'] == $row['id']){
+        $packetlist = file_get_contents('templates/packagelist.html');
+        
+        //If a id have been added to the url it will be checked.
+        if(preg_match('/^\d+$/', $_GET['id']) && $_GET['id'] == $row['id']){
             $packetlist = str_replace('{{selectedpackage}}'," checked",$packetlist);
-          } else {
-             $packetlist = str_replace('{{selectedpackage}}',"",$packetlist);
-          }
-          $packetlist = str_replace('{{packagename}}',$row['name'],$packetlist);
-          $packetlist = str_replace('{{packageid}}',$row['id'],$packetlist);
-          $packetlist = str_replace('{{desc}}',"Prices beginning from " . $price,$packetlist);
-          $listPackage .= $packetlist;
+        } else {
+            $packetlist = str_replace('{{selectedpackage}}',"",$packetlist);
+        }
+        
+        $packetlist = str_replace('{{packagename}}',$row['name'],$packetlist);
+        $packetlist = str_replace('{{packageid}}',$row['id'],$packetlist);
+        $packetlist = str_replace('{{desc}}',"Prices beginning from " . $price,$packetlist);
+        $listPackage .= $packetlist;
      }
 
 	$template = file_get_contents('templates/index.html');
